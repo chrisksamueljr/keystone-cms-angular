2d0783eb2c3443283683164aab215198494c4999
Chris Samuel <chrisk.samueljr@gmail.com>
2019-05-29 16:28:46 -0400

    installed the default code as per DOCS

---
+++ keystone.js
@@ -0,0 +1,18 @@
+/*
+
+keystone.js file is the launch file for Keystone:
+ it defines general configuration options, initialises 
+ Keystone, and starts your application server.
+**/
+
+var keystone = require('keystone');
+
+keystone.init({
+  'cookie secret': 'secure string goes here',
+  	'name': 'keystoneCMS',
+  	'user model': 'User',
+  	'auto update': true,
+  	'auth': true,
+});
+
+keystone.start();
